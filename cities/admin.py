99a6b69525857fc7e70662d4150fd3d70a81773e
Sohrab <plannersohrab@gmail.com>
2022-09-04 22:46:00 +0600

    First commit

---
+++ cities/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import City
+# Register your models here.
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name","state",)
+
+admin.site.register(City, CityAdmin)
